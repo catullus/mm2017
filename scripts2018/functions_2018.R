7572d4c55cd6209cf4b69666844a1ae0f443637a
catullus <cody.flagg@gmail.com>
2018-03-07 19:38:53 -0700

    @jaroberti moving player stats to data2018 folder -- trying to keep 2017 script execution and folder structure stable

---
+++ scripts2018/functions_2018.R
@@ -0,0 +1,43 @@
+ranker <- function(data){
+    
+    # assign week of the year based on Daynum -- add year from 'Season'
+    ## for the given day, assign the numeric week
+    week_idx <- data.frame(day=seq(0, 161, 7), week=seq(1,24, 1))
+    assign_week <- function(d, idx){
+        for (i in 1:length(idx$day)){
+            if (d >= idx[i,1] & d <= idx[i+1,1]){
+                return(idx[i,2])
+            }
+        }
+    }
+    
+    # assign 1 for win, 0 for loss, 0.5 for draw to team in left-most column
+    data$Outcome <- ifelse(data$Wscore - data$Lscore>0, 1, 0)
+    data$Week <- sapply(data$Daynum, assign_week, week_idx)
+    data$weekkey_winner <- paste0(data$Wteam,"_",data$Week)
+    data$weekkey_loser <- paste0(data$Lteam,"_",data$Week)
+    
+    # this uses the Daynum converted to week
+    ranks <- steph(select(data, Week, Wteam, Lteam, Outcome), history=TRUE)
+    
+    # matrix comes in wide format
+    wide_ranks <- as.data.frame(ranks$history)
+    wide_ranks$player <- rownames(wide_ranks)
+    
+    # convert to long format
+    long_ranks <- tidyr::gather(wide_ranks, value = player)
+    names(long_ranks) <- c("player", "type", "value")
+    ## this is screwing up the week -- split is a regex field
+    long_ranks$week <- ldply(stringr::str_split(long_ranks$type,"[.]"))[,1]
+    
+    # only grab ratings
+    weekly_ranks <- filter(long_ranks, str_detect(type, "Rating"))
+    weekly_ranks$weekkey <- paste0(weekly_ranks$player,"_",weekly_ranks$week)
+    
+    # 
+    reg_weekly_ranks <- merge(data, weekly_ranks, by.x=c("weekkey_winner"), by.y=c("weekkey"))
+    reg_weekly_ranks <- rename(reg_weekly_ranks, Wteam_rank=value) %>% select(-player, -type, -week)
+    reg_weekly_ranks <- merge(reg_weekly_ranks, weekly_ranks, by.x=c("weekkey_loser"), by.y=c("weekkey"))
+    reg_weekly_ranks <- rename(reg_weekly_ranks, Lteam_rank=value) %>% select(-player, -type, -week)
+    return(reg_weekly_ranks)
+}
